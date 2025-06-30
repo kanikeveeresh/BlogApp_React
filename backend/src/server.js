33b81c7062385312c699a20d2495004dfaa2d3d8
Kanike Veeresh <veereshlee1821@gmail.com>
2025-06-30 10:17:29 +0530

    Added frontend origin

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: 'https://blogapp-react.onrender.com',
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
